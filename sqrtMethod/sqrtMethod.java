2e399988f7b413f2747de47062c85c574cd80404
ashishgulatiG4G <ashishgulati2112@gmail.com>
2024-06-11 23:43:54 +0530

    Added source code for "sqrtMethod in Java"

---
+++ sqrtMethod/sqrtMethod.java
@@ -0,0 +1,30 @@
+package sqrtMethod;
+
+public class sqrtMethod {
+    public static void main(String[] args) {
+
+
+        System.out.println(Math.sqrt(90.4));
+
+        System.out.println(Math.sqrt(-4.0));
+
+        System.out.println(Math.sqrt(0.0));
+
+        
+        System.out.println(Double.POSITIVE_INFINITY);
+
+        System.out.println(Math.sqrt(Double.NEGATIVE_INFINITY));
+
+
+
+
+    }
+}
+
+/*
+ *  1. Square root of a number 
+ *  2. Sqrt of negative / negative infinity -> NaN
+ *  3. Zero -> Same result
+ *  4. +infinity -> Same result
+ * 5.  
+ */
